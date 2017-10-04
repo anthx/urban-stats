b7440ae842b260dac5bed7ed20f6f68382424b5b
acarrick <anthonix@gmail.com>
2017-10-04 21:07:14 +1000

    improved filtering

--- word_stats.py
+++ word_stats.py
@@ -8,11 +8,13 @@ for n, definition in enumerate(definitions["list"]):
     # print(n," ", definition["definition"], "\n")
     all_words += definition["definition"] + " "
 # print(all_words)
-list_of_common_words = ["is", "a", "the", "to", "and", "you", "of", "not", "for",
-                        "it", "when", "it", "out", "as", "in", "are", "they", "your", "who", "all"]
+list_of_common_words = ["that", "with", "the", "to", "and", "you", "of", "not", "for",
+                        "have", "when", "out", "as", "in", "are", "they", "your", "who", "all"]
 most_words = []
-for word in all_words.split():
-    if word.lower() not in list_of_common_words and word.isalpha():
+for word in all_words.split(" "):
+    # if "." in word: print(word.isalpha())
+    if (word.lower() not in list_of_common_words and
+            not word.isnumeric() and len(word) > 3 and "\n" not in word):
         most_words.append(word)
 # print(definitions)
 frequency = {}
